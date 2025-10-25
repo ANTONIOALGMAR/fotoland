08ae5c7eadab1db0f7b07d53b96eb4fcb4b1d865
antonio algmar <antonioalgmar@MacBook-Pro-de-antonio.local>
2025-10-25 10:11:10 -0300

    feat(backend): Update CORS allowedOrigins for Netlify preview and Render frontend

--- fotoland-backend/src/main/java/com/fotoland/backend/config/WebConfig.java
+++ fotoland-backend/src/main/java/com/fotoland/backend/config/WebConfig.java
@@ -17,7 +17,11 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) { // Add this method
         registry.addMapping("/**") // Apply CORS to all endpoints
-                .allowedOrigins("https://sunny-tiramisu-e5bbdc.netlify.app") // Allow requests from your Netlify frontend
+                .allowedOrigins(
+                        "https://sunny-tiramisu-e5bbdc.netlify.app",
+                        "https://68fccb9d2f47e9b5e1604501--sunny-tiramisu-e5bbdc.netlify.app",
+                        "https://fotoland-frontend.onrender.com"
+                    ) // Allow requests from your Netlify frontend and Render frontend
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allowed HTTP methods
                 .allowedHeaders("*") // Allow all headers
                 .allowCredentials(true); // Allow credentials (e.g., cookies, authorization headers)
