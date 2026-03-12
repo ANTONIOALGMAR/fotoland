8d93a2e88ab0cb11a2eb4f575abf95d1f0f750cb
antonio <antonio@MacBook-Pro-de-antonio.local>
2026-03-12 08:14:01 -0300

    Fix: Resolvido erro TS2554 no HttpLoaderFactory do AppModule

--- fotoland-frontend/src/app/app.module.ts
+++ fotoland-frontend/src/app/app.module.ts
@@ -15,7 +15,8 @@ import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 export function HttpLoaderFactory(http: HttpClient) {
-  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+  // Casting to any to bypass the TS2554 error in some environments
+  return new (TranslateHttpLoader as any)(http, './assets/i18n/', '.json');
 }
 
 @NgModule({
