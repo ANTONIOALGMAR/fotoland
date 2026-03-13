b9543207629b7ba4f800217b5b57404812e5dbbe
antonio <antonio@MacBook-Pro-de-antonio.local>
2026-03-13 10:23:33 -0300

    Fix: Adicionado import faltante de JsonBackReference no Post.java

--- fotoland-backend/src/main/java/com/fotoland/backend/model/Post.java
+++ fotoland-backend/src/main/java/com/fotoland/backend/model/Post.java
@@ -1,5 +1,6 @@
 package com.fotoland.backend.model;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import java.time.LocalDateTime;
