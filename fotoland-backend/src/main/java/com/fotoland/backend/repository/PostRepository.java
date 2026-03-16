a163fa5af1b08b5cff93cfa09d59fe3217027c20
antonio <antonio@MacBook-Pro-de-antonio.local>
2026-03-16 14:15:29 -0300

    Fix deleteByAuthorId query

--- fotoland-backend/src/main/java/com/fotoland/backend/repository/PostRepository.java
+++ fotoland-backend/src/main/java/com/fotoland/backend/repository/PostRepository.java
@@ -4,10 +4,16 @@ import com.fotoland.backend.model.Post;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long>, org.springframework.data.jpa.repository.JpaSpecificationExecutor<Post> {
     Page<Post> findByAlbumId(Long albumId, Pageable pageable);
-    void deleteByAuthorId(Long authorId);
+
+    @Modifying
+    @Query("delete from Post p where p.album.author.id = :authorId")
+    void deleteByAuthorId(@Param("authorId") Long authorId);
 }
